15bb1308d1ee87364fd8a320ea9de03cf5f4a004
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-08-27 14:27:16 +0530

    fix typos
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- cmd/vm/start.go
+++ cmd/vm/start.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -32,7 +32,7 @@ var (
 	mounts []string
 )
 
-// StartCmd represents the preq command
+// StartCmd represents the start command
 var StartCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Start a new VM for Kindli",
